182967104fb4859a4e08681faf5704d8c033b583
SongBirdCode <Songbirdcode@gmail.com>
2024-07-07 14:01:03 -0400

    ENV file for Cloud DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI;
+const client = new MongoClient(mongoURI)
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/letshang');
+
+export function geDB(dbName = process.env.DB_NAME){
+    return Client.db(dbName)
+}
 
 module.exports = mongoose.connection;
